a81421fce46b06757111940453ccb18000482062
Tony Kerstiens <tony.kerstiens@alineops.com>
2024-11-28 10:12:23 -0700

    Add Fibonacci sequence generator
    This commit introduces a Fibonacci sequence generator with a class that provides methods for getting the next Fibonacci number and calculating the nth Fibonacci number. The changes include a new header file (fibonacci.h), implementation file (fibonacci.cpp), and CMakeLists.txt for building the static library.

---
+++ library/fibonacci/fibonacci.h
@@ -0,0 +1,17 @@
+#ifndef FIBONACCI_LIBRARY_H
+#define FIBONACCI_LIBRARY_H
+
+class FibonacciGenerator {
+public:
+    FibonacciGenerator() : a(0), b(1) {}
+    FibonacciGenerator(int a, int b) : a(a), b(b) {}
+    int next();
+    static int get_nth_fibonacci(int n);
+
+private:
+    int a, b;
+};
+
+
+
+#endif //FIBONACCI_LIBRARY_H
