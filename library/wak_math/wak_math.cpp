2e4542dbc6b2c71900b02b8940432b1a2a523536
Tony Kerstiens <tony.kerstiens@alineops.com>
2024-11-28 20:52:04 -0700

    Add documentation for mathematical function declarations
    This commit introduces Doxygen-style comments to the function declarations in wak_math.cpp. These comments provide a brief description of each function's purpose, parameters, and return type, enhancing code readability and maintainability. Accurate documentation will also aid future developers in understanding and implementing these mathematical functions effectively.

--- library/wak_math/wak_math.cpp
+++ library/wak_math/wak_math.cpp
@@ -2,7 +2,12 @@
 #include <vector>
 
 namespace wak_math {
-
+    /**
+     * @brief Get the nth prime number.
+     *
+     * @param n
+     * @return unsigned long long
+     */
     unsigned long long get_nth_prime(const unsigned long long n) {
         if (n == 1) return 2;
         if (n == 2) return 3;
@@ -28,6 +33,12 @@ namespace wak_math {
         return primes[n - 1]; // n-1 because index in vector starts from 0
     }
 
+    /**
+     * @brief Get the sum of all prime numbers less than n.
+     *
+     * @param n
+     * @return unsigned long long
+     */
     unsigned long long sum_primes(const unsigned long long n) {
         if (n < 2) return 0;
 
@@ -72,6 +83,12 @@ namespace wak_math {
         return largest_prime_factor;
     }
 
+    /**
+     * @brief Reverse a number.
+     *
+     * @param n
+     * @return unsigned long long
+     */
     unsigned long long reverse_number(const unsigned long long n) {
         auto reverse = 0ULL;
         auto number = n;
@@ -82,10 +99,22 @@ namespace wak_math {
         return reverse;
     }
 
+    /**
+     * @brief Get the sum of squares of n natural numbers.
+     *
+     * @param n
+     * @return unsigned long long
+     */
     unsigned long long sum_of_squares(const unsigned long long n) {
         return n * (n + 1) * (2 * n + 1) / 6;
     }
 
+    /**
+     * @brief Get the square of sum of n natural numbers.
+     *
+     * @param n
+     * @return unsigned long long
+     */
     unsigned long long square_of_sum(const unsigned long long n) {
         const auto sum = n * (n + 1) / 2;
         return sum * sum;
